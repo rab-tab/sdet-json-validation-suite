998f4d02a29095aca867a148ca47b5c6de64f638
Rabia <sdetcode@gmail.com>
2025-09-29 17:59:52 +0530

    add CachedOrderProvider to reuse loaded orders across tests

---
+++ src/main/java/dataProvider/CachedDataProvider.java
@@ -0,0 +1,33 @@
+package dataProvider;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.jayway.jsonpath.JsonPath;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CachedDataProvider {
+    public static class CachedOrder {
+        public final JsonNode node;
+        public final String json;
+
+        public CachedOrder(JsonNode node) {
+            this.node = node;
+            this.json = node.toString();
+        }
+
+        @Override
+        public String toString() {
+            return JsonPath.read(json, "$.id");
+        }
+    }
+
+    private static final List<CachedOrder> cachedOrders = OrderDataProvider.loadOrders()
+            .map(CachedOrder::new)
+            .collect(Collectors.toList());
+
+    public static Stream<CachedOrder> ordersProvider() {
+        return cachedOrders.stream();
+    }
+}
